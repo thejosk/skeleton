063030f5fc2d93a7e93fc7bcf7a54c72f48e03ff
Thejo <thejokondubhatla@sullivancotter.com>
2018-05-07 09:52:09 -0500

    add upcoming interview schedule for an interviewer

--- app/models/interviewer.rb
+++ app/models/interviewer.rb
@@ -2,4 +2,21 @@ class Interviewer < ApplicationRecord
   has_many :answer_ratings
   has_many :interviews
 
+  def upcoming_interview_schedule
+    upcoming_interviews = interviews.where('interview_date >= ?', DateTime.now).order(:interview_date).first(2)
+    upcoming_interviews.each do |i|
+      puts "Candidate Information :"
+      puts "Name : #{i.candidate.name}"
+      puts "Email : #{i.candidate.email}"
+      puts "Phone : #{i.candidate.phone}"
+      puts "Interview Date : #{i.interview_date}"
+      puts "\n"
+      puts "List of Questions :"
+      i.questions.each do |q|
+        puts "#{q.question}"
+      end
+      puts "\n\n"
+    end
+  end
+
 end
